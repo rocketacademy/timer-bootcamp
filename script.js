e69ab72cb706fc605cdf5ae4e7dead92dccb8df9
alvinljt <alvinljt@gmail.com>
2020-10-26 16:21:52 +0800

    implemented laps and split section

--- script.js
+++ script.js
@@ -13,10 +13,31 @@ const MIN_PER_HR = 3;
 // stores ID of CountTimeId function
 let timerId;
 
+// to store total number of seconds since timer started till end of previous lap.
+let totalSecondsTillPrevLap = 0;
+// to store total number of seconds since timer started.
+let totalSeconds = 0;
+// object to store current lap time
+const lapTime = {
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+};
+// array to store lap times
+const lapTimes = [];
+// element to display heading for lap time container
+let lapTimeContainerHeading;
+// element to display current lap time
+let lapTimeEl;
+// container to display lap times
+let lapTimeNumbersContainer;
+// container to display all information related to lap time
+let lapTimeContainer;
+
 // container to display elapsed time
 let elapsedTimeContainer;
 
-// start, stop, reset and lap button elements
+// elements to display start, stop, reset and lap buttons
 let startButton;
 let stopButton;
 let resetButton;
@@ -33,6 +54,15 @@ const createStartingElements = () => {
   stopButton = document.createElement('button');
   resetButton = document.createElement('button');
   lapButton = document.createElement('button');
+
+  // element to display heading for lap time container
+  lapTimeContainerHeading = document.createElement('h2');
+  // element to display current lap time
+  lapTimeEl = document.createElement('p');
+  // container to display all information related to lap time
+  lapTimeNumbersContainer = document.createElement('div');
+  // container to display all information related to lap time
+  lapTimeContainer = document.createElement('div');
 };
 
 // to create a string about elapsed time in hours, minutes and seconds
@@ -41,6 +71,7 @@ const getElapsedTimeOutput = () => `${elapsedTime.hours}:H ${elapsedTime.minutes
 // to start the time and keep track of it
 const countTime = () => {
   const countTimeId = setInterval(() => {
+    totalSeconds += 1;
     elapsedTime.seconds += 1;
 
     // increase minutes and hours
@@ -56,9 +87,6 @@ const countTime = () => {
       }
     }
 
-    // store current time in the lap array
-    // lapArray.push(elapsedTime);
-
     // store elapsed time output in container that displays elapsed time
     elapsedTimeContainer.innerText = getElapsedTimeOutput();
   }, 1000);
@@ -67,33 +95,100 @@ const countTime = () => {
 
 // Game initilization =====================================================
 createStartingElements();
+
 elapsedTimeContainer.innerText = getElapsedTimeOutput();
-document.body.appendChild(elapsedTimeContainer);
+
+lapTimeContainerHeading.innerText = 'Lap Times';
 
 startButton.innerText = 'start';
 stopButton.innerText = 'stop';
 resetButton.innerText = 'reset';
 lapButton.innerText = 'lap';
 startButton.addEventListener('click', () => {
-  console.log('start button pressed');
+  // start or continue the timer
   timerId = countTime();
 });
 stopButton.addEventListener('click', () => {
+  // stop the timer
   console.log('stop button pressed');
   clearInterval(timerId);
 });
 resetButton.addEventListener('click', () => {
-  console.log('reset button pressed');
+  // stop the timer
   clearInterval(timerId);
-  elapsedTime.seconds += 1;
+  // reset elapsed time to zero
   elapsedTime.seconds = 0;
   elapsedTime.minutes = 0;
   elapsedTime.hours = 0;
-
   elapsedTimeContainer.innerText = getElapsedTimeOutput();
+
+  // remove the display of all lap times
+  lapTimeNumbersContainer.innerHTML = '';
 });
-// lapButton.addEventListener('click');
+lapButton.addEventListener('click', () => {
+  if (lapTimes.length === 0) {
+    // if it is the 1st lap, just display current elapsed time -------------------
+    // store elapsed time as the 1st lap time
+    lapTime.seconds = elapsedTime.seconds;
+    lapTime.minutes = elapsedTime.minutes;
+    lapTime.hours = elapsedTime.hours;
+
+    // store 1st lap time to array of lap times
+    lapTimes.push(lapTime);
+
+    // create lap time element to display current lap time
+    lapTimeEl = document.createElement('p');
+    lapTimeEl.innerText = `${lapTime.hours}:H ${lapTime.minutes}:M ${lapTime.seconds}:S`;
+    lapTimeNumbersContainer.appendChild(lapTimeEl);
+
+    // store current total seconds for use as previous total seconds during next lap
+    totalSecondsTillPrevLap = totalSeconds;
+  } else if (lapTimes.length > 0) {
+    // if it is the 2nd lap onwards: ----------------------------------------------
+    // calculate current lap's total seconds by
+    // subtracting current total seconds since timer started by previous total seconds
+    const currentLapTotalSeconds = totalSeconds - totalSecondsTillPrevLap;
+
+    // convert current lap's total seconds into seconds, min, hrs
+    const currentLapSeconds = currentLapTotalSeconds % SEC_PER_MIN;
+    let currentLapMinutes = 0;
+    console.log('currentLapTotalSeconds' + currentLapTotalSeconds);
+    if (currentLapTotalSeconds >= SEC_PER_MIN) {
+      // only find lap mins if the lap's total seconds is more than a min's worth of seconds
+      currentLapMinutes = Math.floor(currentLapTotalSeconds / SEC_PER_MIN) % MIN_PER_HR;
+      console.log('count min' + Math.floor(currentLapTotalSeconds / SEC_PER_MIN));
+    }
+    let currentLapHours = 0;
+    if (currentLapTotalSeconds >= (SEC_PER_MIN * MIN_PER_HR)) {
+      // only find lap hrs if the lap's total seconds is more than an hr's worth of seconds
+      currentLapHours = Math.floor(currentLapTotalSeconds / (SEC_PER_MIN * MIN_PER_HR));
+      console.log('count hours' + currentLapHours);
+    }
+
+    // store converted current lap time format into current lap time
+    lapTime.seconds = currentLapSeconds;
+    lapTime.minutes = currentLapMinutes;
+    lapTime.hours = currentLapHours;
+
+    // store current lap time to array of lap times
+    lapTimes.push(lapTime);
+
+    // create lap time element to display current lap time
+    lapTimeEl = document.createElement('p');
+    lapTimeEl.innerText = `${lapTime.hours}:H ${lapTime.minutes}:M ${lapTime.seconds}:S`;
+    lapTimeNumbersContainer.appendChild(lapTimeEl);
+
+    // store current total seconds for use as previous total seconds during next lap
+    totalSecondsTillPrevLap = totalSeconds;
+  }
+});
+
+// append starting elements and containers
+document.body.appendChild(elapsedTimeContainer);
 document.body.appendChild(startButton);
 document.body.appendChild(stopButton);
 document.body.appendChild(resetButton);
 document.body.appendChild(lapButton);
+lapTimeContainer.appendChild(lapTimeContainerHeading);
+lapTimeContainer.appendChild(lapTimeNumbersContainer);
+document.body.appendChild(lapTimeContainer);
