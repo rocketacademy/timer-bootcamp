a08eed0ba8bad1f0b59eb8e0160bd9c66188222e
ZAF07 <zaffere07@gmail.com>
2021-04-16 12:07:11 +0800

    Added Reset and lap function

--- script.js
+++ script.js
@@ -3,47 +3,52 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let stopTimerValue = false;
-
 // QUERY DOM ELEMENTS
-const startButton = document.getElementById("start");
+const startTimerButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
-const resetButton = document.getElementById("reset");
+const resetTimerButton = document.getElementById("reset");
 const lapButton = document.getElementById("lap");
 const displaySecondsOnScreen = document.getElementById("show-seconds");
 const displayMinuteOnScreen = document.getElementById("show-minute");
 const displayHourOnScreen = document.getElementById("show-hour");
-
+const displayLapOnScreen = document.getElementById("lap-list");
 // FUNCTION TO START TIMER
 const startTimer = () => {
   stopTimerValue === true ? (stopTimerValue = false) : true;
+  minutes = 3;
+  seconds = 0;
   const toStartTimer = setInterval(() => {
+    console.log("timer started");
     displaySecondsOnScreen.innerText = seconds;
     displayMinuteOnScreen.innerText = minutes;
     displayHourOnScreen.innerText = hours;
-    seconds += 1;
     if (stopTimerValue === true) {
       console.log("STOP CLICKED", stopTimerValue);
       console.log("Value of stopTimerValue", stopTimerValue);
       clearInterval(toStartTimer);
-    } else if (seconds === 60) {
-      minutes += 1;
+    } else if (seconds === 0 && minutes != 0) {
+      minutes -= 1;
       console.log("minute ", minutes);
-      seconds = 0;
+      seconds = 60;
       console.log("second ", minutes);
       //IF MINUTES == 60 ? HOURS === +=1 MINUTES === 0
-    } else if (minutes === 60) {
-      hours += 1;
-      minutes = 0;
+    } else if (minutes === 0 && seconds === 0) {
+      // hours += 1;
+      // minutes = 0;
+      showMsgFunc("Game about to end timer message", "red");
+      clearInterval(toStartTimer);
+      setTimeout(() => {
+        location.reload();
+      }, 5000);
     }
+    seconds -= 1;
   }, 1000);
 };
-
 // FUNCTION TO STOP TIMER
 const stopTimer = () => {
   console.log("Change value to true");
   stopTimerValue = true;
 };
-
 // FUNCTION TO RESET TIMER
 const resetTimer = () => {
   console.log("RESET WAS CLICKED");
@@ -53,9 +58,18 @@ const resetTimer = () => {
   seconds = 0;
   minutes = 0;
   hours = 0;
+  displaySecondsOnScreen.innerText = seconds;
+  displayMinuteOnScreen.innerText = minutes;
+  displayHourOnScreen.innerText = hours;
 };
-
+const lapTimer = () => {
+  console.log("lap was clicked");
+  const laps = document.createElement("li");
+  laps.innerText = `${hours}:${minutes}:${seconds}`;
+  displayLapOnScreen.appendChild(laps);
+};
 // USER CLICK LISTENER
-startButton.addEventListener("click", startTimer);
+startTimerButton.addEventListener("click", startTimer);
 stopButton.addEventListener("click", stopTimer);
-resetButton.addEventListener("click", resetTimer);
+resetTimerButton.addEventListener("click", resetTimer);
+lapButton.addEventListener("click", lapTimer);
